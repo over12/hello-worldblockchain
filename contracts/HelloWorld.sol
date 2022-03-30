eeacaad16621dc43ea7091b9fa77a4ad0eb22567
Roni Sommerfeld <roni@must.pt>
2022-03-30 14:41:49 +0100

    new resources to publish blockchain

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -18,14 +18,13 @@ contract HelloWorld {
   function sayHello() public view returns(string memory){
     return greeting;
   }
-  
+
   /**
   * Método para alterar o texto na variavel greeting
   * quando queremos recuperar valores de variavies usaremos o termo "view"
   * ele deixa claro para blockchain que esse método apenas retornar valores e nao alterará
   * métodos view não tem taxas de gas, ou seja, é gratuito.
-  * @param greeting string
-  * @return void
+  * @param _greeting string  
    */
   function updateGreeting(string memory _greeting) public{
     greeting = _greeting;
